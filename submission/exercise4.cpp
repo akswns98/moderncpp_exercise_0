0f0ec1adc9dcdd48b404fafd469974cc0b0f1f4a
akswns98 <99632038+akswns98@users.noreply.github.com>
2023-12-18 12:26:20 +0900

    Create exercise4.cpp

---
+++ submission/exercise4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class MyBaseClass {
+public:
+	virtual void Hello() {
+		std::cout << "This is Base\n";
+	}
+
+	virtual ~MyBaseClass() {
+		std::cout << "~MyBaseClass\n";
+	}
+};
+
+class MyDerivedClass : public MyBaseClass {
+public:
+	void Hello() override {
+		std::cout << "This is Derived\n";
+	}
+	~MyDerivedClass() {
+		std::cout << "~MyDerivedClass\n";
+	}
+};
+int main() {
+	std::unique_ptr<MyDerivedClass> ptr = std::make_unique<MyDerivedClass>();
+
+	ptr->Hello();
+	
+
+	return 0;
+}
